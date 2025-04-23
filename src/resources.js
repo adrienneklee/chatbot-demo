a7076800190557772be63b1317d10f5ca7f47083
“ADRIENNE <adrienneklee@gmail.com>
2025-04-23 15:20:44 -0400

    added some styling and removed header

--- src/resources.js
+++ src/resources.js
@@ -8,11 +8,14 @@ export const helpOptions = [
 
 export const helpMessages = {
     "Healthcare": `Healthcare is a top concern for many Canadians. 6.5 million Canadians don't have access to primary care. The NDP will guarantee that every Canadian can access primary care and a family doctor by 2030.`,
+    
     "Affordability": `The NDP will introduce emergency price caps on basic food items—like pasta, frozen vegetables, and infant formula—to keep costs of food down. These items have seen some of the steepest increases since 2020: cooking oil is up 75 per cent, pasta is up 43 per cent, and infant formula—an essential for so many parents—is up 30 per cent and still climbing.`,
+
+    "The Environment": `The impacts of accelerating climate change are all around us—and Canadians are paying the price. The NDP will pass an Environmental Bill of Rights and establish an Office of Environmental Justice to address the disproportionate impacts of pollution and loss of biodiversity on Indigenous, Black and racialized communities, as well as youth and future generations.`,
+    
 }
 
 export const helpLinks = {
-    "The Environment": "https://calgarysfuture.ca/",
 	"Housing": "https://calgarysfuture.ca/actions/sign-on-for-housing/",
 	"Education": "https://calgarysfuture.ca/actions/safe_schools/"
 };
